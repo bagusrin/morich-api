3bb9c18437f041c2a5d7ee784b9b624695d1a8b0
bagusrin <bagusrinn@gmail.com>
2018-06-05 22:35:03 +0700

    update api video admin list for cms

--- build/app/routes/videoadmin.js
+++ build/app/routes/videoadmin.js
@@ -4,6 +4,7 @@ module.exports = {
   configure: function (app) {
     app.route('/video/admin/list').get(video.list);
     app.route('/video/admin/list-slider').get(video.listSlider);
+    app.route('/video/admin/list_').get(video.listAdmin);
     app.route('/video/admin/detail/:id').get(video.detail);
     app.route('/video/admin/post').post(video.post);
     app.route('/video/admin/update').post(video.update);
